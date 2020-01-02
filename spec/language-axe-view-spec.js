4cb0798ca62a9dd2b36734e64b5b687bd96e869d
Francis Ilechukwu <francis94c@gmail.com>
2020-01-02 20:47:31 +0100

    Initial Commit.

---
+++ spec/language-axe-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageAxeView from '../lib/language-axe-view';
+
+describe('LanguageAxeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
